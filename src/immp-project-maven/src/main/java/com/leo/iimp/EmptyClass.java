380cd6fc0f3618d2b4973e74164398a271e3f120
fangchi <fangchi1986@yahoo.com.cn>
2013-05-02 10:33:17 +0800

    删除不需要的文件

--- src/immp-project-maven/src/main/java/com/leo/iimp/EmptyClass.java
+++
@@ -1,5 +0,0 @@
-package com.leo.iimp;
-
-public class EmptyClass {
-
-}
--- src/immp-project-maven/src/test/java/com/leo/iimp/EmptyTestCase.java
+++
@@ -1,7 +0,0 @@
-package com.leo.iimp;
-
-import junit.framework.TestCase;
-
-public class EmptyTestCase extends TestCase {
-
-}
